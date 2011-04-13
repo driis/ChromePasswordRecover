c538c9521d801d9899d2a7c4fcb4a333a99e3ae8
Dennis Riis <dr@driis.dk>
2011-04-13 20:23:39 +0200

    Made ChromePasswordRecover independent of the old dr.Commons project reference

---
+++ ConsoleUtility/BaseProgram.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+namespace dr.Commons.ConsoleUtility
+{
+    /// <summary>
+    /// Base console program class that abstracts away commonly needed functionality for console programs.
+    /// </summary>
+    /// <remarks>
+    /// To use, inherit from BaseProgram and implement the RunProgram method.
+    /// </remarks>
+    public abstract class BaseProgram
+    {        
+        /// <summary>
+        /// Command line.
+        /// </summary>
+        private LazyField<CommandLineParser> commandLine;        
+
+        /// <summary>
+        /// Gets the author name.
+        /// </summary>
+        /// <value>The author.</value>
+        public virtual string Author
+        {
+            get
+            {
+                return "Dennis Riis";
+            }
+        }
+
+        /// <summary>
+        /// Gets the copyright.
+        /// </summary>
+        /// <value>The copyright.</value>
+        public virtual string Copyright
+        {
+            get
+            {
+                return String.Format("{0} - Copyright (c) {1}, {2}",ProductName, Author, DateTime.Now.Year);
+            }
+        }
+
+        /// <summary>
+        /// Gets the name of the product.
+        /// </summary>
+        /// <value>The name of the product.</value>
+        public virtual string ProductName
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Main routine.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="constructor">The constructor.</param>
+        /// <param name="args">The args.</param>
+        public static void Main<T>(Func<T> constructor, string [] args)
+            where T : BaseProgram
+        {
+            try
+            {
+                if ( constructor == null )
+                    throw new ArgumentNullException("constructor");
+                
+                var program = constructor();
+                program.commandLine = LazyField.Get(() => new CommandLineParser(args));
+                program.DisplayCopyright();
+                program.RunProgram(args);
+            }
+            catch(Exception ex)
+            {
+                DisplayException(ex);
+            }
+        }
+
+        /// <summary>
+        /// Displays the copyright message.
+        /// </summary>
+        public virtual void DisplayCopyright()
+        {
+            Console.WriteLine(Copyright);
+            Console.WriteLine();
+        }
+        /// <summary>
+        /// Displays the exception.
+        /// </summary>
+        /// <param name="ex">The ex.</param>
+        protected static void DisplayException(Exception ex)
+        {
+            using(new ConsoleColorSection(ConsoleColor.Red))
+            {
+                Console.WriteLine("\nException occured:");
+                DisplayInnerException(ex, 0);
+            }
+        }
+
+        /// <summary>
+        /// Displays the friendly error.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        protected static void DisplayFriendlyError(string message)
+        {
+            DisplayFriendlyError(message,null);
+        }
+
+        /// <summary>
+        /// Displays the friendly error.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="inserts">The inserts.</param>
+        protected static void DisplayFriendlyError(string message, params object[] inserts)
+        {
+            using(new ConsoleColorSection(ConsoleColor.Red))
+            {
+                if ( inserts == null )
+                    Console.WriteLine(message);
+                else
+                    Console.WriteLine(message,inserts);
+
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Displays the inner exception.
+        /// </summary>
+        /// <param name="ex">The ex.</param>
+        /// <param name="count">The count.</param>
+        private static void DisplayInnerException(Exception ex, int count)
+        {
+            if (ex == null)
+                return;
+
+            if (count > 0)
+                using (new ConsoleColorSection(ConsoleColor.DarkRed))
+                    Console.WriteLine("InnerException #{0}", count);
+            Console.WriteLine("{0}: {1}", ex.GetType().Name, ex.Message);
+            Console.WriteLine("Stack Trace:");
+            Console.WriteLine(ex.StackTrace);
+            Console.WriteLine();
+            DisplayInnerException(ex.InnerException, count + 1);
+        }
+
+        /// <summary>
+        /// Runs the program.
+        /// </summary>
+        /// <param name="args">The args.</param>
+        public abstract void RunProgram(string[] args);
+
+        /// <summary>
+        /// Gets the command line.
+        /// </summary>
+        /// <value>The command line.</value>
+        public CommandLineParser CommandLine
+        {
+            get { return commandLine; }
+        }
+    }    
+}
---
+++ ConsoleUtility/CommandLineParser.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace dr.Commons.ConsoleUtility
+{
+    /// <summary>
+    /// Command-line parsing helper.
+    /// </summary>
+    public class CommandLineParser 
+    {
+        /// <summary>
+        /// Prefixes for switches.
+        /// </summary>
+        private readonly char[] switchPrefixes = new[] {'-', '/'};
+        /// <summary>
+        /// The arguments as received in the constructor
+        /// </summary>
+        private readonly string[] args;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandLineParser"/> class.
+        /// </summary>
+        /// <param name="args">The args.</param>
+        public CommandLineParser(string [] args)
+        {
+            this.args = args;
+        }
+
+        /// <summary>
+        /// The arguments.
+        /// </summary>
+        public IEnumerable<string> Arguments
+        {
+            get
+            {
+                return from a in args
+                       where !String.IsNullOrEmpty(a) && !switchPrefixes.Contains(a.First())
+                       select a;
+            }
+        }
+
+        /// <summary>
+        /// Gets the switches.
+        /// </summary>
+        /// <value>The switches.</value>
+        public IEnumerable<string> Switches
+        {
+            get
+            {
+                return from a in args
+                       where !String.IsNullOrEmpty(a) && switchPrefixes.Contains(a.First())
+                       select a.Substring(1);
+            }
+        }
+
+        /// <summary>
+        /// Gets the argument.
+        /// </summary>
+        /// <param name="argumentIndex">Index of the argument.</param>
+        /// <returns></returns>
+        public string GetArgument(int argumentIndex)
+        {
+            return Arguments.Skip(argumentIndex).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Determines whether the specified switch name has switch.
+        /// </summary>
+        /// <param name="switchName">Name of the switch.</param>
+        /// <returns>
+        /// 	<c>true</c> if the specified switch name has switch; otherwise, <c>false</c>.
+        /// </returns>
+        public bool HasSwitch(string switchName)
+        {           
+            string sw = GetNamedSwitch(switchName);
+            return null != sw;
+        }
+
+        /// <summary>
+        /// Gets the switch value.
+        /// </summary>
+        /// <param name="switchName">Name of the switch.</param>
+        /// <returns></returns>
+        public string GetSwitchValue(string switchName)
+        {
+            string sw = GetNamedSwitch(switchName);
+            if (sw == null)
+                return null;
+            string[] parts = sw.Split(':');
+            if ( parts.Length < 2)
+                return null;
+
+            return parts[1];
+        }
+
+        /// <summary>
+        /// Gets all the files specified on the command line.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<FileInfo> GetFiles()
+        {
+            var specs = from arg in Arguments
+                        let dirPart = Path.GetDirectoryName(arg)
+                        select
+                            new
+                                {
+                                    file = Path.GetFileName(arg),
+                                    directory = String.IsNullOrEmpty(dirPart) ? Environment.CurrentDirectory : dirPart
+                                };
+            var files = specs.SelectMany(s => Directory.GetFiles(s.directory, s.file)).Where(File.Exists);
+            return files.Select(f => new FileInfo(f));
+        }
+        /// <summary>
+        /// Gets the named switch.
+        /// </summary>
+        /// <param name="switchName">Name of the switch.</param>
+        /// <returns></returns>
+        private string GetNamedSwitch(string switchName)
+        {
+            if (switchName == null)
+                throw new ArgumentNullException("switchName");
+
+            return (from s in Switches
+                   where s.StartsWith(switchName, StringComparison.OrdinalIgnoreCase)
+                   select s).FirstOrDefault();
+        }
+    }
+}
